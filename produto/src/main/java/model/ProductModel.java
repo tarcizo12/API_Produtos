3b415d45bdec55082e4495089a808e6b2b633499
tarcizo <tarcizon12@gmail.com>
2023-01-24 08:29:24 -0300

    Criando classe model

---
+++ produto/src/main/java/model/ProductModel.java
@@ -0,0 +1,31 @@
+package model;
+
+public class ProductModel {
+    private int code;
+    private String name;
+    private double value;
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}

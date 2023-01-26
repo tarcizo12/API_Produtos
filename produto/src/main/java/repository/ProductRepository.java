82dd47288538e185da9f951c8cf45874e5c5fb2c
tarcizo <tarcizon12@gmail.com>
2023-01-26 10:49:44 -0300

    Criando interface repositorio

---
+++ produto/src/main/java/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package repository;
+
+import model.ProductModel;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository <ProductModel, Integer> {
+        //Lista produtos
+        List<ProductModel> findAll();
+
+        //Pesquisar por id
+        ProductModel findById(int primaryKey);
+
+        //Remover produto
+        void delete(ProductModel product);
+
+        //Cadastrar ou alterar produto
+        <ProdMod extends ProductModel> ProdMod  save(ProductModel product);
+}
